5d9a32354b685f11dad3e5111d0c7c0d4a2d91aa
SASCHADDD <saschadanu23@gmail.com>
2026-04-17 10:01:41 +0700

    membuat event TaptoFocuss

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
 sealed class CameraEvent{}
 
 final class initializeCamera extends CameraEvent{}
 final class SwutchCamera extends CameraEvent{}
 final class ToggleFlash extends CameraEvent{}
 final class TakePicture extends CameraEvent{
-  final void function(File imagefile) onPictuteTaken;
-  TakePicture(this.onPictureTaken);
+  final void Function(File imageFile) onPictuteTaken;
+  TakePicture(this.onPictuteTaken);
+}
+final class TapToFocus extends CameraEvent{
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
 }
