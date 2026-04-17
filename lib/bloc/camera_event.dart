35b9016dc4086381be6ec5c626d0bba46015d0ea
SASCHADDD <saschadanu23@gmail.com>
2026-04-17 10:05:40 +0700

    membuat state memilih foto dari gallery dan membuka camera dan capture

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -2,17 +2,27 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-sealed class CameraEvent{}
+sealed class CameraEvent {}
 
-final class initializeCamera extends CameraEvent{}
-final class SwutchCamera extends CameraEvent{}
-final class ToggleFlash extends CameraEvent{}
-final class TakePicture extends CameraEvent{
+final class initializeCamera extends CameraEvent {}
+
+final class SwutchCamera extends CameraEvent {}
+
+final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
   final void Function(File imageFile) onPictuteTaken;
   TakePicture(this.onPictuteTaken);
 }
-final class TapToFocus extends CameraEvent{
+
+final class TapToFocus extends CameraEvent {
   final Offset position;
   final Size previewSize;
   TapToFocus(this.position, this.previewSize);
+}
+
+final class PickImageFromGallery extends CameraEvent {}
+final class OpenCameraAndCapture extends CameraEvent {
+  final BuildContext context;
+  OpenCameraAndCapture(this.context);
 }
